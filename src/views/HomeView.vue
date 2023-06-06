752d38fbec7b44184e466d4efe4e73904c560a4b
Wesley Leal <wesley7leal@outlook.com>
2023-06-06 13:29:45 -0300

    WIP esse commit altera a Vue home dando um conselhoa aletório em um modal

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,36 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+<script>
+export default {
+    data() {
+        return {
+            adv: [],
+        };
+    },
+    methods: {
+        getAdvice(url) {
+            fetch(url)
+            .then((res) => res.json())
+            .then((data) => {
+                this.adv = data;
+            });
+        },
+    },
+    mounted(){
+        this.getAdvice("https://api.adviceslip.com/advice");
+    }
+}
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+    <main>
+
+    
+    <table>
+    <tr>
+    <th>Advice</th>
+    </tr>
+    <tr v-for="conselho in adv" :key="conselho.advice"> 
+    <td>{{ conselho.advice }}</td>
+    </tr>
+    </table>
+    </main>
 </template>
