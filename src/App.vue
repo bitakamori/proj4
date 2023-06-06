c2339338ac10132bf84432162b18e1b5aa62c26b
Bianca <biancahelenatakamoriribeiro@gmail.com>
2023-06-06 13:29:40 -0300

    routerlink para o search

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/Search">Search Advice</RouterLink>
       </nav>
     </div>
   </header>
