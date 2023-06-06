72c9aa6f846bc53f2b99126bfcd04e3e0eb4f3d4
Bianca <biancahelenatakamoriribeiro@gmail.com>
2023-06-06 13:30:28 -0300

    criação view search

---
+++ src/views/SearchView.vue
@@ -0,0 +1,34 @@
+<script>
+export default {
+    data() {
+        return {
+            adv: [],
+        };
+    },
+    methods: {
+        getAdvice() {
+            fetch("https://api.adviceslip.com/advice")
+            .then((res) => res.json())
+            .then((data) => {
+                this.adv = data;
+            });
+        },
+    },
+    mounted(){
+        this.getAdvice();
+    }
+}
+</script>
+
+<template>
+    <main>
+    <table>
+    <tr>
+    <th>Advice</th>
+    </tr>
+    <tr v-for="conselho in adv" :key="conselho.advice"> 
+    <td>{{ conselho.advice }}</td>
+    </tr>
+    </table>
+    </main>
+</template>
