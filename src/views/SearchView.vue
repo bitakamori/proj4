db13efa46e2caf66933e189a555e3f4752d2a0be
Bianca <biancahelenatakamoriribeiro@gmail.com>
2023-06-06 15:53:25 -0300

    correções search

--- src/views/SearchView.vue
+++ src/views/SearchView.vue
@@ -1,34 +1,47 @@
 <script>
 export default {
-    data() {
-        return {
-            adv: [],
-        };
+  data() {
+    return {
+      searchQuery: "",
+      adviceList: [],
+      advice: "",
+      //temOutrosAdvices: false,
+    };
+  },
+  methods: {
+    searchAdvice() {
+      const apiUrl = `https://api.adviceslip.com/advice/search/${this.searchQuery}`;
+
+      fetch(apiUrl)
+        .then((response) => response.json())
+        .then((data) => {
+            if (data.slips && data.slips.length > 0) {
+            this.advice = data.slips[0].advice;
+          } else {
+            this.advice = 'Today everything is gonna be alright';
+          }
+        })
+        .catch((error) => {
+          console.error(error);
+        });
     },
-    methods: {
-        getAdvice() {
-            fetch("https://api.adviceslip.com/advice")
-            .then((res) => res.json())
-            .then((data) => {
-                this.adv = data;
-            });
-        },
-    },
-    mounted(){
-        this.getAdvice();
-    }
-}
+  },
+};
 </script>
 
 <template>
-    <main>
-    <table>
-    <tr>
-    <th>Advice</th>
-    </tr>
-    <tr v-for="conselho in adv" :key="conselho.advice"> 
-    <td>{{ conselho.advice }}</td>
-    </tr>
-    </table>
-    </main>
+  <div>
+    <input
+      type="text"
+      v-model="searchQuery"
+      placeholder="Say a word and i give u an advice :)"
+    />
+    <button @click="searchAdvice">Buscar</button>
+    <p>{{
+       advice
+      }}</p> 
+    <!-- <button :disabled="!temOutrosAdvices"></button> -->
+  </div>
 </template>
+
+
